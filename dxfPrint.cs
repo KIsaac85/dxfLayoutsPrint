8f68ddb794efba2545309e1c91f8c00a3d169159
Karim <karim_isaac@hotmail.com>
2023-04-16 01:07:08 +0200

    layouts are retrieved

--- dxfPrint.cs
+++ dxfPrint.cs
@@ -19,148 +19,41 @@ namespace dxfLayoutsPrint
         [CommandMethod("dxfp")]
         public void printDXF()
         {
-            //Get the current document and database
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Database database = doc.Database;
-            
-            // Get the layout dictionary of the current database
-            using (Transaction acadTrans = database.TransactionManager.StartTransaction())
-            {
-                //Get the name of layouts
-                DBDictionary lays =
-                    acadTrans.GetObject(database.LayoutDictionaryId,
-                        OpenMode.ForRead) as DBDictionary;
+            // Get the current document and database, and start a transaction
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
 
-                doc.Editor.WriteMessage("\nLayouts:");
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Reference the Layout Manager
                 LayoutManager acLayoutMgr = LayoutManager.Current;
+
+
+
+                DBDictionary lays =
+             acTrans.GetObject(acCurDb.LayoutDictionaryId,
+                 OpenMode.ForRead) as DBDictionary;
+
+                acDoc.Editor.WriteMessage("\nLayouts:");
+
                 // Step through and list each named layout and Model
                 foreach (DBDictionaryEntry item in lays)
                 {
-
-                    Layout acLayout = acadTrans.GetObject(acLayoutMgr.GetLayoutId(item.Key),
-                                                    OpenMode.ForRead) as Layout;
-                    doc.Editor.WriteMessage("\n  " + acLayout.LayoutName);
-                    using (PlotInfo plotInfo = new PlotInfo())
+                   // acDoc.Editor.WriteMessage("\n  " + item.Key);
+                    
+                    // Get the current layout and output its name in the Command Line window
+                    Layout acLayout = acTrans.GetObject(item.m_value,OpenMode.ForRead) as Layout;
+                    
+                    if (acLayout.LayoutName!="Model")
                     {
-                        plotInfo.Layout = acLayout.ObjectId;
-                        // Get a copy of the PlotSettings from the layout
-                        using (PlotSettings plotSettings = new PlotSettings(acLayout.ModelType))
-                        {
-                            plotSettings.CopyFrom(acLayout);
-                            // Update the PlotSettings object
-                            PlotSettingsValidator plotSettingsVdr = PlotSettingsValidator.Current;
-
-                            // Set the plot type
-                            plotSettingsVdr.SetPlotType(plotSettings, Autodesk.AutoCAD.DatabaseServices.PlotType.Extents);
-
-                            // Set the plot scale
-                            plotSettingsVdr.SetUseStandardScale(plotSettings, true);
-                            plotSettingsVdr.SetStdScaleType(plotSettings, StdScaleType.ScaleToFit);
-
-                            // Center the plot
-                            plotSettingsVdr.SetPlotCentered(plotSettings, true);
-
-                            // Set the plot device to use
-                            plotSettingsVdr.SetPlotConfigurationName(plotSettings, "DWF6 ePlot.pc3", "ANSI_A_(8.50_x_11.00_Inches)");
-
-                            // Set the plot info as an override since it will
-                            // not be saved back to the layout
-                            plotInfo.OverrideSettings = plotSettings;
-
-                            // Validate the plot info
-                            using (PlotInfoValidator plotInfoVdr = new PlotInfoValidator())
-                            {
-                                plotInfoVdr.MediaMatchingPolicy = MatchingPolicy.MatchEnabled;
-                                //plotInfoVdr.Validate(plotInfo);
-
-                                // Check to see if a plot is already in progress
-                                if (PlotFactory.ProcessPlotState == ProcessPlotState.NotPlotting)
-                                {
-                                    using (PlotEngine acPlEng = PlotFactory.CreatePublishEngine())
-                                    {
-                                        // Track the plot progress with a Progress dialog
-                                        using (PlotProgressDialog acPlProgDlg = new PlotProgressDialog(false, 1, true))
-                                        {
-                                            using ((acPlProgDlg))
-                                            {
-                                                // Define the status messages to display 
-                                                // when plotting starts
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.DialogTitle, "Plot Progress");
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.CancelJobButtonMessage, "Cancel Job");
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.CancelSheetButtonMessage, "Cancel Sheet");
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.SheetSetProgressCaption, "Sheet Set Progress");
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.SheetProgressCaption, "Sheet Progress");
-
-                                                // Set the plot progress range
-                                                acPlProgDlg.LowerPlotProgressRange = 0;
-                                                acPlProgDlg.UpperPlotProgressRange = 100;
-                                                acPlProgDlg.PlotProgressPos = 0;
-
-                                                // Display the Progress dialog
-                                                acPlProgDlg.OnBeginPlot();
-                                                acPlProgDlg.IsVisible = true;
-
-                                                // Start to plot the layout
-                                                acPlEng.BeginPlot(acPlProgDlg, null);
-
-                                                // Define the plot output
-                                                acPlEng.BeginDocument(plotInfo, acLayout.LayoutName, null, 1, true, "c:\\myplot");
-
-                                                // Display information about the current plot
-                                                acPlProgDlg.set_PlotMsgString(PlotMessageIndex.Status, "Plotting: " + acLayout.LayoutName + " - " + acLayout.LayoutName);
-
-                                                // Set the sheet progress range
-                                                acPlProgDlg.OnBeginSheet();
-                                                acPlProgDlg.LowerSheetProgressRange = 0;
-                                                acPlProgDlg.UpperSheetProgressRange = 100;
-                                                acPlProgDlg.SheetProgressPos = 0;
-
-                                                // Plot the first sheet/layout
-                                                using (PlotPageInfo acPlPageInfo = new PlotPageInfo())
-                                                {
-                                                    acPlEng.BeginPage(acPlPageInfo, plotInfo, true, null);
-                                                }
-
-                                                acPlEng.BeginGenerateGraphics(null);
-                                                acPlEng.EndGenerateGraphics(null);
-
-                                                // Finish plotting the sheet/layout
-                                                acPlEng.EndPage(null);
-                                                acPlProgDlg.SheetProgressPos = 100;
-                                                acPlProgDlg.OnEndSheet();
-
-                                                // Finish plotting the document
-                                                acPlEng.EndDocument(null);
-
-                                                // Finish the plot
-                                                acPlProgDlg.PlotProgressPos = 100;
-                                                acPlProgDlg.OnEndPlot();
-                                                acPlEng.EndPlot(null);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        acDoc.Editor.WriteMessage("\n  " + acLayout.LayoutName);
+                        
                     }
-                
                 }
 
                 // Abort the changes to the database
-                acadTrans.Abort();
-
-
-
-                //print the required layouts
-
-
-                // Reference the Layout Manager
-                
-
-                // Get the current layout and output its name in the Command Line window
-                
+                acTrans.Abort();
             }
-            
         }
 
         #endregion
