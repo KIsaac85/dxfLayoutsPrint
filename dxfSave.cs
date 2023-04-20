4ce17c65ec8bd36fc1d5066edcce8f904509273f
Karim <karim_isaac@hotmail.com>
2023-04-20 11:51:09 +0200

    The file is organised

--- dxfSave.cs
+++ dxfSave.cs
@@ -1,41 +1,39 @@
 ﻿using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.AutoCAD.PlottingServices;
 
-namespace dxfLayoutsPrint
+
+namespace dxfFileOut
 {
-    public class dxfPrint : IExtensionApplication
+    public class dxfSave : IExtensionApplication
     {
         #region Commands
+        public DocumentCollection documents { get; set; }
+        public string dwgName { get; set; }
+        public string dxfName { get; set; }
 
-        [CommandMethod("dxfp")]
+
+
+        [CommandMethod("dxfs")]
         public void printDXF()
         {
 
 
             // Get the current document and database, and start a transaction
-            DocumentCollection documents = Application.DocumentManager;
+            documents = Application.DocumentManager;
 
             foreach (Document item in documents)
             {
                 //Database acCurDb = acDoc.Database;
-                string dwgName = item.Name;
-                using (var db = new Database(false, true))
+                dwgName = item.Name;
+                using (Database db = new Database(false, true))
                 {
                     db.ReadDwgFile(dwgName, FileOpenMode.OpenForReadAndAllShare, true, null);
-                    string dxfName = System.IO.Path.ChangeExtension(dwgName, "dxf");
-                    db.DxfOut(dxfName, 16, true);
+                    dxfName = System.IO.Path.ChangeExtension(dwgName, "dxf");
+                    db.DxfOut(dxfName, 16, DwgVersion.AC1027);
+                    
                 } 
             }
-
             
         }
 
