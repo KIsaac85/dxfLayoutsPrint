f50804ffdbdf83373a481b37f173c1e78c5cf3a6
Karim <karim_isaac@hotmail.com>
2023-04-26 18:45:36 +0200

    Rev01 better user experience. A message pops up when files are created

--- dxfSave.cs
+++ dxfSave.cs
@@ -1,7 +1,7 @@
 ﻿using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
-
+using Autodesk.AutoCAD.EditorInput;
 
 namespace dxfFileOut
 {
@@ -12,29 +12,39 @@ namespace dxfFileOut
         public string dwgName { get; set; }
         public string dxfName { get; set; }
 
-
+        public Editor ed { get; set; }
 
         [CommandMethod("dxfs")]
         public void printDXF()
         {
 
 
-            // Get the current document and database, and start a transaction
-            documents = Application.DocumentManager;
-
-            foreach (Document item in documents)
+            try
             {
-                //Database acCurDb = acDoc.Database;
-                dwgName = item.Name;
-                using (Database db = new Database(false, true))
+                // Get the current document and database, and start a transaction
+                documents = Application.DocumentManager;
+
+                foreach (Document item in documents)
                 {
-                    db.ReadDwgFile(dwgName, FileOpenMode.OpenForReadAndAllShare, true, null);
-                    dxfName = System.IO.Path.ChangeExtension(dwgName, "dxf");
-                    db.DxfOut(dxfName, 16, DwgVersion.AC1027);
-                    
-                } 
+                    //Database acCurDb = acDoc.Database;
+                    dwgName = item.Name;
+                    using (Database db = new Database(false, true))
+                    {
+                        db.ReadDwgFile(dwgName, FileOpenMode.OpenForReadAndAllShare, true, null);
+                        dxfName = System.IO.Path.ChangeExtension(dwgName, "dxf");
+                        db.DxfOut(dxfName, 16, DwgVersion.AC1027);
+
+                    }
+                }
+                ed = Application.DocumentManager.MdiActiveDocument.Editor;
+                ed.WriteMessage("\nFile(s) Created Successfully.");
+
             }
-            
+            catch (System.Exception)
+            {
+
+                ed.WriteMessage("\nAn error has occured. Please contact the provider");
+            }
         }
 
         #endregion
